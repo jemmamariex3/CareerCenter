7edcbf4920a3aeb4a7127d77d02772c42b533ddc
Jemma Marie <jemma.tiongson.420@my.csun.edu>
2019-01-14 10:19:54 -0800

    pushing changes to create new Users Branch

--- app/section/skillsContent/Counseling.js
+++ app/section/skillsContent/Counseling.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {SafeAreaView,TouchableOpacity} from 'react-native'
+
 import Icon from 'react-native-vector-icons/FontAwesome'
 
 export class Counseling extends Component {
