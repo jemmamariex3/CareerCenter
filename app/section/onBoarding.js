369b5715a4fa74d6f2c287258b4300f49a990417
Jemma Marie <jemma.tiongson.420@my.csun.edu>
2019-02-11 12:34:19 -0800

    made custom slides with pngs

--- app/section/onBoarding.js
+++ app/section/onBoarding.js
@@ -1,71 +1,172 @@
 import React, {Component} from 'react';
-import { StyleSheet,View, Text, SafeAreaView } from 'react-native';
+import { StyleSheet,View, Text, SafeAreaView, Image, Dimensions} from 'react-native';
 import AppIntroSlider from 'react-native-app-intro-slider';
-import {Users} from "../views/Users";
+import LinearGradient from 'react-native-linear-gradient';
 
 const styles = StyleSheet.create({
+    mainContent: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+        padding:0,
+        width: Dimensions.get("window").width,
+        height: Dimensions.get("window").height,
+    },
+    imgContainer:{
+        width: 500,
+        height: 300,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: "#ffffff",
+
+    },
     explore: {
         width: 315,
         height: 208,
+        margin: 20
     },
     counsel:{
         width: 307,
         height: 216,
+        margin: 20
+
     },
     resource:{
         width: 282,
         height: 236,
+        margin: 20
+
     },
     connect:{
         width: 312,
-        height: 178,
+        height: 200,
+        margin: 20
+
     },
     ready:{
         width: 312,
         height: 178,
+        margin: 20
+
     },
     direction:{
         width: 311,
         height: 215,
+        margin: 20
+
     },
     events:{
         width: 281,
         height: 160,
+        margin: 20
+
     },
     cinthy:{
         width: 297,
         height: 191,
+        margin: 20
+
     },
     eLearning:{
         width: 258,
         height: 215,
+        margin: 20
+
     },
     pin:{
         width: 25,
         height: 25
     },
-    title:{
+    exploreTitle:{
+        color:'#F8BD58',
         fontWeight: 'bold',
-        color:'#5b4d90',
         textTransform: 'uppercase',
-        fontSize:14,
+        fontSize:24,
         textAlign: 'center'
     },
-    info:{
-        textAlign: 'center',
-        color: '#E84C37',
-        fontWeight: 'bold'
+    counselTitle:{
+        color:'#E84C37',
+        fontWeight: 'bold',
+        textTransform: 'uppercase',
+        fontSize:24,
+        textAlign: 'center'
     },
-    subInfo:{
-        fontSize: 12
+    resourceTitle:{
+        color:'#91A1CC',
+        fontWeight: 'bold',
+        textTransform: 'uppercase',
+        fontSize:24,
+        textAlign: 'center'
     },
-    onBoard:{
-        marginLeft: -20,
+    connectionTitle:{
+        color:'#2EA4DC',
+        fontWeight: 'bold',
+        textTransform: 'uppercase',
+        fontSize:24,
+        textAlign: 'center'
+    },
+    readinessTitle:{
+        color:'#5C4C92',
+        fontWeight: 'bold',
+        textTransform: 'uppercase',
+        fontSize:24,
+        textAlign: 'center'
+    },
+    directionTitle:{
+        color:'#82CFCA',
+        fontWeight: 'bold',
+        textTransform: 'uppercase',
+        fontSize:24,
+        textAlign: 'center'
+    },
+    eventsTitle:{
+        color:'#b95bef',
+        fontWeight: 'bold',
+        textTransform: 'uppercase',
+        fontSize:24,
+        textAlign: 'center'
+    },
+    cinthyTitle:{
+        color:'#87ddf4',
+        fontWeight: 'bold',
+        textTransform: 'uppercase',
+        fontSize:24,
+        textAlign: 'center'
+    },
+    learningTitle:{
+        color:'#7ee77a',
+        fontWeight: 'bold',
+        textTransform: 'uppercase',
+        fontSize:24,
+        textAlign: 'center'
+    },
+    service:{
+        width: 216,
+        height: 41,
+    },
+    event:{
+        width: 207,
+        height: 47,
+    },
+    cinthyBanner:{
+        width: 200,
+        height: 47,
+    },
+    learn:{
+        width: 315,
+        height: 48,
+    },
+    logo:{
+        width: 231,
+        height: 23,
+        margin: 30
     },
     text:{
         color: '#ffffff',
         fontSize:14,
         margin: 20,
+        marginTop: 30,
         fontWeight: "bold"
     }
 });
@@ -76,92 +177,163 @@ const slides = [
         title: 'Self-Exploration',
         text: 'The Career Center offers career services to all currently enrolled CSUN students and eligible alumni',
         image: require('../img/Exploration/Alvin_SE_illustration_vector_015-05.png'),
+        banner:require('../img/serviceBanner.png'),
+        logo:require('../img/cc_logo_white.png'),
         imageStyle: styles.explore,
-        backgroundColor: '#F6BA61',
-        textStyle:styles.text,
-    },
+        bannerStyle: styles.service,
+        logoStyle: styles.logo,
+        colors: ['#F48E29', '#FCC74D'],
+        textStyle: styles.text,
+        titleStyle: styles.exploreTitle
+
+},
     {
         key: 2,
         title: 'Counseling',
         text: 'The Career Center offers career services to all currently enrolled CSUN students and eligible alumni',
         image: require('../img/Counseling/Alvin_SE_illustration_vector_015-04.png'),
-        imageStyle: styles.connect,
-        backgroundColor: '#E84C3D',
+        banner:require('../img/serviceBanner.png'),
+        logo:require('../img/cc_logo_white.png'),
+        imageStyle: styles.counsel,
+        bannerStyle: styles.service,
+        logoStyle: styles.logo,
+        colors: ['#E84C37', '#E67D2F'],
         textStyle:styles.text,
+        titleStyle: styles.counselTitle
     },
     {
         key: 3,
         title: 'Resources',
         text: 'The Career Center offers career services to all currently enrolled CSUN students and eligible alumni',
         image: require('../img/Resources/Alvin_SE_illustration_vector_015-06.png'),
+        banner:require('../img/serviceBanner.png'),
+        logo:require('../img/cc_logo_white.png'),
         imageStyle: styles.resource,
-        backgroundColor: '#8297CA',
+        bannerStyle: styles.service,
+        logoStyle: styles.logo,
+        colors: ['#6078DD', '#91A1CC'],
         textStyle:styles.text,
+        titleStyle: styles.resourceTitle
     },
     {
         key: 4,
         title: 'Connections',
         text: 'The Career Center offers career services to all currently enrolled CSUN students and eligible alumni',
         image: require('../img/Connections/Alvin_SE_illustration_vector_015-08.png'),
+        banner:require('../img/serviceBanner.png'),
+        logo:require('../img/cc_logo_white.png'),
         imageStyle: styles.connect,
-        backgroundColor: '#59b2ab',
+        bannerStyle: styles.service,
+        logoStyle: styles.logo,
+        colors: ['#2EA4DC', '#70DAF4'],
         textStyle:styles.text,
+        titleStyle: styles.connectionTitle
+
     },
     {
         key: 5,
         title: 'Readiness',
         text: 'The Career Center offers career services to all currently enrolled CSUN students and eligible alumni',
         image: require('../img/Readiness/Alvin_SE_illustration_vector_015-07.png'),
+        banner:require('../img/serviceBanner.png'),
+        logo:require('../img/cc_logo_white.png'),
+        colors: ['#5C4C92', '#6972E3'],
         imageStyle: styles.ready,
-        backgroundColor: '#5B4D90',
+        bannerStyle: styles.service,
+        logoStyle: styles.logo,
         textStyle:styles.text,
+        titleStyle: styles.readinessTitle
+
     },
     {
         key: 6,
         title: 'Direction',
         text: 'The Career Center offers career services to all currently enrolled CSUN students and eligible alumni',
         image: require('../img/Direction/Alvin_SE_illustration_vector_015-09.png'),
+        banner:require('../img/serviceBanner.png'),
+        logo:require('../img/cc_logo_white.png'),
+        colors: ['#82CFCA', '#84D6AD'],
         imageStyle: styles.direction,
-        backgroundColor: '#84CDC9',
+        bannerStyle: styles.service,
+        logoStyle: styles.logo,
         textStyle:styles.text,
+        titleStyle: styles.directionTitle
+
     },
     {
         key: 7,
         title: 'Events',
         text: 'The Career Center hosts a broad array of Career Fairs, Employer Info Sessions, and Workshops/Programs. For full details and how to reserve your slot for Info Session, Workshops and Networking Events, login to SUNlink.',
         image: require('../img/Events/eventsLogo.png'),
-        imageStyle: styles.events,
-        backgroundColor: '#C655F0',
+        banner:require('../img/eventsBanner.png'),
+        logo:require('../img/cc_logo_white.png'),
+        colors: ['#4E92EF', '#C655F0'],
+        imageStyle: styles.ready,
+        bannerStyle: styles.event,
+        logoStyle: styles.logo,
         textStyle:styles.text,
+        titleStyle: styles.eventsTitle
+
     },
     {
         key: 8,
         title: 'Cinthy',
         text: 'Chat with the Career Center mascot Cinthy and ask her any questions you have.',
         image: require('../img/Cinthy/cinthyLogo.png'),
+        banner:require('../img/cinthyBanner.png'),
+        logo:require('../img/cc_logo_white.png'),
+        colors: ['#FFE838', '#81DDFF'],
         imageStyle: styles.cinthy,
-        backgroundColor: '#81DDFF',
+        bannerStyle: styles.cinthyBanner,
+        logoStyle: styles.logo,
         textStyle:styles.text,
+        titleStyle: styles.cinthyTitle
+
     },
     {
         key: 9,
         title: 'E-Learning',
         text: 'Access tutorials and videos to learn more about the Career Center.',
         image: require('../img/Elearning/elearning.png'),
+        banner:require('../img/learningBanner.png'),
+        logo:require('../img/cc_logo_white.png'),
+        colors: ['#76E85B', '#69D7FF'],
         imageStyle: styles.eLearning,
-        backgroundColor: '#76E85B',
+        bannerStyle: styles.learn,
+        logoStyle: styles.logo,
         textStyle:styles.text,
+        titleStyle: styles.learningTitle
     },
 ];
 export class onBoarding extends Component {
     static navigationOptions = {
         header:null
     };
+    _renderItem = props => (
+        <LinearGradient
+            style={[styles.mainContent, {
+                width: props.width,
+                height: props.height,
+            }]}
+            colors={props.colors}
+            start={{x: 0, y: .1}} end={{x: .1, y: 1}}
+        >
+            <SafeAreaView style={styles.mainContent}>
+                <Image source={props.logo} style={props.logoStyle}/>
+                <Image source={props.banner} style={props.bannerStyle}/>
+                <View style={styles.imgContainer}>
+                    <Image source={props.image} style={props.imageStyle}/>
+                    <Text style={props.titleStyle}>{props.title}</Text>
+                </View>
+                <Text style={props.textStyle}>{props.text}</Text>
+            </SafeAreaView>
+        </LinearGradient>
+    );
 
     render(){
         const { navigate } = this.props.navigation;
         return(
-                <AppIntroSlider slides={slides} onDone={() => navigate('Tabs')} showSkipButton={true} onSkip={() => navigate('Tabs')}/>
+                <AppIntroSlider slides={slides} renderItem={this._renderItem} onDone={() => navigate('Tabs')} showSkipButton={true} onSkip={() => navigate('Tabs')}/>
         )
 
     }
