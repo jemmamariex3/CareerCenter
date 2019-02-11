af6e23eb151f73b9a62c3a6e5075c529b183819d
Jemma Marie <jemma.tiongson.420@my.csun.edu>
2019-02-11 14:24:47 -0800

    fixed bug with videos

--- android/app/src/main/java/com/careercenter/MainApplication.java
+++ android/app/src/main/java/com/careercenter/MainApplication.java
@@ -3,6 +3,7 @@ package com.careercenter;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.BV.LinearGradient.LinearGradientPackage;
 import com.inprogress.reactnativeyoutube.ReactNativeYouTube;
 import com.oblador.vectoricons.VectorIconsPackage;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new LinearGradientPackage(),
             new ReactNativeYouTube(),
             new VectorIconsPackage(),
             new SplashScreenReactPackage()
